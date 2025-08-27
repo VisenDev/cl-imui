4cceaed1c68d9e4e8674be24dd00db6114f23def
Robert Burnett <dr.slate2@gmail.com>
2025-08-27 14:29:41 -0500

    refactored codebase to use classes instead of structs

--- cl-imui.lisp
+++ cl-imui.lisp
@@ -1,13 +1,16 @@
-(defmacro ui-box (ui params &body body)
-  (declare (ignore ui params))
-  body
+;(defmacro ui-box (ui params &body body)
+;  (declare (ignore ui params))
+;  body
+                                        ;  )
+
+(defconstant +vec-default-capacity+ 32)
+(defun make-vec (type)
+  (make-array +vec-default-capacity+
+              :element-type type :adjustable t :fill-pointer 0)
   )
 
 
-(defvar *ui* nil)
-
 (deftype u8 () '(integer 0 255))
-
 (defstruct (color (:conc-name "col-")
                   (:constructor make-color (&optional r g b a))
                   )
@@ -37,16 +40,110 @@
    (theme (make-theme) :type theme)
    (margin 5 :type fixnum)
    (font-id (make-font) :type font)
+   )
+
+(defclass cmd ()
+  ((x :initarg :x :initform 0 :type fixnum :accessor x)
+   (y :initarg :y :initform 0 :type fixnum :accessor y)
+   (color :initarg :color :type color :accessor color)
+   )
   )
- 
-(defstruct ui
-  (opt (make-opt) :type opt)
-  (x 0 :type fixnum)
-  (y 0 :type fixnum)
-  (layout-direction-stack '())
+(defclass cmd/text (cmd)
+  ((text :initarg :text :type string :accessor text)
+   )
+  )
+(defclass cmd/rect (cmd)
+  ((width :initarg :width :type fixnum :accessor width)
+   (height :initarg :height :type fixnum :accessor height)
+   )
   )
 
-(defun 
+(defclass container ()
+  ((root-x :initarg :root-x :initform 0 :accessor root-x :type fixnum)
+   (root-y :initarg :root-y :initform 0 :accessor root-y :type fixnum)
+   (min-width :initarg :min-width :initform 0 :accessor min-width :type fixnum)
+   (max-width :initarg :max-width :initform #xFFFFFF :accessor max-width :type fixnum)
+   (min-height :initarg :min-height :initform 0 :accessor min-height :type fixnum)
+   (max-height :initarg :max-height :initform #xFFFFFF :accessor max-height :type fixnum)
+   (used-width  :initform 0 :accessor used-width :type fixnum)
+   (used-height :initform 0 :accessor used-height :type fixnum)
+   (x :initarg :x :initform 0 :accessor x :type fixnum)
+   (y :initarg :y :initform 0 :accessor y :type fixnum)
+   (cmds :initform (make-vec 'cmd) :accessor cmds :type (vector cmd))
+   )
+  )
+(defclass container/row (container)
+  ()
+  )
+
+;;; TODO define a container-push function
+
+ 
+(defstruct ui
+  (x 0 :type fixnum)
+  (y 0 :type fixnum)
+  (cmds (make-vec 'cmd))
+  (containers (make-vec 'container))
+  )
+
+;(defun ui-current-zindex (ui)
+;  "returns the zindex of the top container"
+;  (fill-pointer (ui-containers ui))
+;  )
+(defun ui-current-container (ui)
+  (aref (ui-containers ui) (1- (fill-pointer (ui-containers ui))))
+  )
+(defun ui-current-direction (ui)
+  "returns the direction that the ui is currently being laid out in"
+  (cont-direction (ui-current-container ui))
+  )
+
+(declaim (ftype (function (ui raw-cmd fixnum) t) ui-push-cmd))
+(defun ui-push-cmd (ui cmd zindex)
+  (vector-push-extend (make-cmd :zindex zindex :raw-cmd cmd) (ui-cmds ui))
+  )
+
+(defun ui-occupy (ui width height)
+  "tells the ui that a given amount of space has been used the current container"
+  (case (ui-current-direction ui)
+    (:horizontal (incf (ui-x ui) width))
+    (:vertical (incf (ui-y ui) height))
+    )
+  (
+  )
+
+(defun label (ui text font-name font-size)
+  (ui-push-cmd ui (make-cmd-text :x (ui-x ui) :y (ui-y ui) :value text :font-name font-name :font-size font-size)
+               (fill-pointer (ui-containers ui))
+               )
+  ;; TODO figure out how to measure text
+  (ui-occupy ui 100 10)
+  )
+(defun row-begin (ui)
+  (vector-push-extend (make-container :direction :horizontal) (ui-containers ui))
+  )
+(defun row-end (ui)
+  (let*
+      ((top (vector-pop (ui-containers ui)))
+       (row-x (cont-origin-x top))
+       (row-y (cont-origin-y top))
+       (row-w (ui-x ui))
+       (row-h 50) ;;;TODO calculate this in a better way
+       (row-color (make-color)) ; TODO make this actually check the style
+       ;;;TODO handle margins here
+       )
+    (setf (ui-x ui) row-x)
+    (incf (ui-y ui) row-h)
+    (ui-push-cmd
+     ui
+     (make-cmd-rect :x row-x :y row-y :w row-w :h row-h :color row-color)
+     (fill-pointer (ui-containers ui))
+     )
+    )
+  )
+  
+    
+    
 
 (defmacro rows (ui opt &body body)
    `(
